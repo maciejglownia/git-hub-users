375c6dd7a8a72ec3cadaf44b70d95fd008c47086
Maciej Głownia <maciej.k.glownia@gmail.com>
2024-08-10 16:59:28 +0200

    Initial commit

---
+++ src/main/java/com/glownia/maciej/githubusers/GitHubUsersApplication.java
@@ -0,0 +1,13 @@
+package com.glownia.maciej.githubusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubUsersApplication.class, args);
+	}
+
+}
