e53f76c49eee66fb9278446608fbdc10adef72e0
mglownia <maciej.k.glownia@gmail.com>
2024-08-11 23:04:37 +0200

    Add Constants to keep constant globally

---
+++ src/main/java/com/glownia/maciej/githubusers/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.glownia.maciej.githubusers.utils;
+
+public class Constants {
+
+    public static final String GITHUB_API_URL = "https://api.github.com/";
+    public static final String USERNAME_PATH = "users/{username}/repos";
+    public static final String BRANCHES_PATH = "repos/{username}/{repoName}/branches";
+
+}
